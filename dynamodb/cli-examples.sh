9fbad78cfdfa54b6fe6330adeda500e1bd36feaf
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-05-23 14:27:31 -0600

    update filter expression

--- dynamodb/cli-examples.sh
+++ dynamodb/cli-examples.sh
@@ -4,7 +4,7 @@
 aws dynamodb scan --table-name UserPosts --projection-expression "user_id, content"  --region us-east-1
 
 # Demo Filter Expression
-aws dynamodb scan --table-name UserPosts --filter-expression "user_id = :u" --expression-attribute-values '{ ":u": {"S":"124usersoi3"}}' --region us-east-1
+aws dynamodb scan --table-name UserPosts --filter-expression "user_id = :u" --expression-attribute-values '{ ":u": {"S":"test"}}' --region us-east-1
 
 # Page Size demo: will do 1 API call if you have 3 Items
 aws dynamodb scan --table-name UserPosts --region us-east-1
